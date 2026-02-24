6a3e3b3a6391157493efe7610b208ef9b6baa346
Philippe Martin <philippeoliviermartin@gmail.com>
2026-02-23 22:18:33 -0500

    devops: only allowed local ip to ssh

--- devops/main.py
+++ devops/main.py
@@ -2,8 +2,8 @@ from src.instance_manager import instance_manager as im
 from src.database_manager import database_manager as dm
 from src.parameter_manager import parameter_manager as pm
 
-instance = im.create_backend_instance()
-print(instance.public_ip_address, instance.private_ip_address)
+# instance = im.create_backend_instance()
+# print(instance.public_ip_address, instance.private_ip_address)
 
 # dm.create_database()
 
