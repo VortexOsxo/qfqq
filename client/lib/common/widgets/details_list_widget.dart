f56c9c23ff6cf30f76203362c1e051deb3599df1
Philippe Martin <philippeoliviermartin@gmail.com>
2026-02-28 18:25:57 -0500

    created details list widget to have the same style as details attribute widget

---
+++ client/lib/common/widgets/details_list_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:qfqq/generated/l10n.dart';
+
+class DetailsListWidget extends StatelessWidget {
+  final String label;
+  final String emptyLabel;
+  final List<String> values;
+
+  const DetailsListWidget({
+    super.key,
+    required this.label,
+    required this.emptyLabel,
+    required this.values,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final loc = S.of(context);
+    if (values.isEmpty) {
+      return Text(loc.attributeNoThemes);
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('$label: ', style: TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 4),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:
+              values
+                  .map(
+                    (value) =>
+                        Text('• $value', style: const TextStyle(fontSize: 16)),
+                  )
+                  .toList(),
+        ),
+        const SizedBox(height: 8),
+      ],
+    );
+  }
+}
