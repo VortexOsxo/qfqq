88436c5af4e4c9b326f3ea089bb59cd82474d572
Philippe Martin <philippeoliviermartin@gmail.com>
2026-03-02 11:08:32 -0500

    removed unsued datafilter and data updater abstract class

--- server/flaskr/database/data_updater.py
+++
@@ -1,7 +0,0 @@
-from abc import ABC, abstractmethod
-
-class DataUpdater(ABC):
-    @abstractmethod
-    def update_query(self, query: object) -> object:
-        """ Add the update of the field to the db query"""
-        pass
