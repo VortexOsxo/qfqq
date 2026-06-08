b448ecaed4d310f87341e317dc7df56221ac5f18
Philippe Martin <philippeoliviermartin@gmail.com>
2026-06-08 07:36:38 -0400

    android settings

--- client/lib/mobile/mobile_router.dart
+++ client/lib/mobile/mobile_router.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:qfqq/common/templates/navigation_guard.dart';
 import 'package:qfqq/common/models/meeting_agenda.dart';
