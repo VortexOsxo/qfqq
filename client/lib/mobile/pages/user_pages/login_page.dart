c8124b4d90317db6ad4b105b680e129ad7f1d88c
Philippe Martin <philippeoliviermartin@gmail.com>
2026-06-09 08:32:12 -0400

    added auth page layouts

--- client/lib/mobile/pages/user_pages/login_page.dart
+++ client/lib/mobile/pages/user_pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:qfqq/common/pages/user_pages/login_page_view_model.dart';
 import 'package:qfqq/common/theme/styles.dart';
 import 'package:qfqq/generated/l10n.dart';
+import 'package:qfqq/mobile/layouts/auth_page_layout.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -19,44 +20,43 @@ class _MobileLoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final loc = S.of(context);
-    return Padding(
-      padding: const EdgeInsets.all(24),
-      child: Form(
-        key: vm.formKey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: loc.attributeEmail),
-              onSaved: vm.saveEmail,
-            ),
-            const SizedBox(height: 8),
-            TextFormField(
-              decoration: InputDecoration(labelText: loc.attributePassword),
-              obscureText: true,
-              onSaved: vm.savePassword,
-            ),
-            const SizedBox(height: 8),
-            const SizedBox(height: 16),
-            if (vm.error.isNotEmpty)
-              Text(vm.error, style: const TextStyle(color: Colors.red)),
-            ElevatedButton(
-              style: squareButtonStyle(context),
-              onPressed: vm.submit,
-              child: Text(loc.loginPageButtonLogin),
-            ),
-            const SizedBox(height: 32),
-            TextButton(
-              onPressed: vm.goToSignup,
-              child: Text(loc.loginPageLinkSignup),
-            ),
-            TextButton(
-              onPressed: vm.goToForgottenPassword,
-              child: Text(loc.loginPageForgottenPasswordLink),
-            ),
-          ],
-        ),
+    final form = Form(
+      key: vm.formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            decoration: InputDecoration(labelText: loc.attributeEmail),
+            onSaved: vm.saveEmail,
+          ),
+          const SizedBox(height: 8),
+          TextFormField(
+            decoration: InputDecoration(labelText: loc.attributePassword),
+            obscureText: true,
+            onSaved: vm.savePassword,
+          ),
+          const SizedBox(height: 8),
+          const SizedBox(height: 16),
+          if (vm.error.isNotEmpty)
+            Text(vm.error, style: const TextStyle(color: Colors.red)),
+          ElevatedButton(
+            style: squareButtonStyle(context),
+            onPressed: vm.submit,
+            child: Text(loc.loginPageButtonLogin),
+          ),
+          const SizedBox(height: 32),
+          TextButton(
+            onPressed: vm.goToSignup,
+            child: Text(loc.loginPageLinkSignup),
+          ),
+          TextButton(
+            onPressed: vm.goToForgottenPassword,
+            child: Text(loc.loginPageForgottenPasswordLink),
+          ),
+        ],
       ),
     );
+
+    return AuthPageLayout(child: form);
   }
 }
