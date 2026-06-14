8299b11cc2584dead634bfc980eb3fd0ccff320c
Philippe Martin <philippeoliviermartin@gmail.com>
2026-06-14 06:38:09 -0400

    added agenda filter widget

--- client/lib/common/pages/agenda_list_page.dart
+++ client/lib/common/pages/agenda_list_page.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:qfqq/common/theme/styles.dart';
 import 'package:qfqq/common/view_models/agenda_list_page_view_model.dart';
+import 'package:qfqq/common/widgets/agendas/agenda_filter_widget.dart';
 import 'package:qfqq/common/widgets/decisions/agendas_list_widget.dart';
-import 'package:qfqq/common/widgets/dropdowns/agenda_status_dropdown_menu.dart';
-import 'package:qfqq/common/widgets/dropdowns/project_dropdown_menu.dart';
-import 'package:qfqq/common/widgets/reusables/default_text_field.dart';
 import 'package:qfqq/generated/l10n.dart';
-
 
 class AgendasListPage extends StatelessWidget {
   const AgendasListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AgendaListPageViewModel(
-      builder: (vm) => _AgendaPageView(vm: vm)
-    );
+    return AgendaListPageViewModel(builder: (vm) => _AgendaPageView(vm: vm));
   }
 }
 
@@ -24,41 +19,17 @@ class _AgendaPageView extends StatelessWidget {
 
   const _AgendaPageView({required this.vm});
 
-
   @override
   Widget build(BuildContext context) {
-    final agendas = vm.filteredAgendas;
-
     return Column(
       children: [
-        _buildSearchAndFilterSection(context),
-        Expanded(
-          child: AgendasListWidget(
-            agendas: agendas,
-            animatorName: vm.animatorName,
-            goToAgenda: vm.goToAgenda,
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSearchAndFilterSection(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
+        Padding(
+          padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Expanded(
-                flex: 6,
-                child: DefaultTextField(
-                  onChanged: vm.onSearchQueryChanged,
-                  hintText: S.of(context).searchTitleIdHint,
-                ),
-              ),
-              Expanded(flex: 1, child: SizedBox()),
+              Expanded(flex: 6, child: AgendaFilterWidget(vm: vm)),
+              const Expanded(flex: 1, child: SizedBox()),
               ElevatedButton(
                 onPressed: vm.goToAgendaCreation,
                 style: squareButtonStyle(context),
@@ -66,22 +37,15 @@ class _AgendaPageView extends StatelessWidget {
               ),
             ],
           ),
-          SizedBox(height: 8),
-          Row(
-            children: [
-              AgendaStatusDropdownMenu(
-                initialStatus: vm.statusQuery,
-                valueChanged: vm.onStatusQueryChanged,
-              ),
-              SizedBox(width: 16),
-              ProjectDropdownMenu(
-                initialProject: vm.projectIdQuery,
-                valueChanged: vm.onProjectQueryChanged,
-              ),
-            ],
+        ),
+        Expanded(
+          child: AgendasListWidget(
+            agendas: vm.filteredAgendas,
+            animatorName: vm.animatorName,
+            goToAgenda: vm.goToAgenda,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
