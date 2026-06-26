72504ed9c3df4bd2d7c8ccda2162427240d247d5
Philippe Martin <philippeoliviermartin@gmail.com>
2026-06-26 14:15:25 -0400

    added firebase config

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:qfqq/common/utils/platform.dart';
 import 'package:qfqq/desktop/desktop_app.dart';
+import 'package:qfqq/firebase_options.dart';
 import 'package:qfqq/mobile/mobile_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   if (platformType == PlatformType.desktop) {
     runApp(ProviderScope(child: const DesktopApp()));
   } else if (platformType == PlatformType.mobile) {
